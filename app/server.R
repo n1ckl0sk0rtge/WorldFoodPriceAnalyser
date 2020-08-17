0a7718ac5d9902e9e99ff0d329d17f4bc2b91ae3
Nicklas Körtge <nicklas-koertge@web.de>
2020-08-17 16:33:29 +0200

    try to implement interactive world map

--- app/server.R
+++ app/server.R
@@ -1,19 +1,120 @@
-library(shiny)
+# source: https://stackoverflow.com/questions/48432061/turn-states-on-a-map-into-clickable-objects-in-shiny
+
+# install necessary packages
+# install.packages( pkgs = c( "devtools", "shiny", "shinydashboard" ) )
+
+# load necessary packages
+library( leaflet )    
+library( shiny )
+library( shinydashboard )
+library(maps)
 
 # data <- read.csv("./../wfp_market_food_prices_inUSD.csv")
 
+# import City of Chicago current community area boundaries
+comarea606 <- readRDS( gzcon( url( description = "https://github.com/cenuno/shiny/raw/master/cps_locator/Data/raw-data/comarea606_raw.RDS" ) ) )
+# Note: for speed, I loaded the GeoJSON file from the City's
+#       data portal and exported the object as an RDS file in another script.
+#       To download the raw data yourself, feel free to run this:
+#       install.packages( pkgs = c( "sp", "rgdal" ) )
+#       comarea606 <- 
+#           rgdal::readOGR( dsn = "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=GEOJSON"
+#                              , layer = "OGRGeoJSON"
+#                              , stringsAsFactors = FALSE
+#                             ) 
+
+worldMap = map("world", fill = TRUE, plot = FALSE)
+print(typeof(worldMap))
+print(sapply(worldMap,'[[',1))
+
+
 server <- function(input, output, session) {
   
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points())
+  # create foundational map
+  foundational.map <- shiny::reactive({
+    leaflet(data = worldMap) %>%
+      addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
+      addPolygons(fillColor = "yellow", stroke = FALSE) %>%
+       # setView( lng = -87.567215
+       #         , lat = 41.822582
+       #         , zoom = 11 ) %>%
+      addPolygons( data = worldMap
+                   , fillOpacity = 0
+                   , opacity = 0.2
+                   , color = "#000000"
+                   , weight = 2
+                   , group = "click.list"
+      )
   })
+  
+  output$myMap <- renderLeaflet({
+    
+    foundational.map()
+    
+  }) # end of leaflet::renderLeaflet({})
+  
+  # store the list of clicked polygons in a vector
+  click.list <- shiny::reactiveValues( ids = vector() )
+  # observe where the user clicks on the leaflet map
+  # during the Shiny app session
+  # Courtesy of two articles:
+  # https://stackoverflow.com/questions/45953741/select-and-deselect-polylines-in-shiny-leaflet
+  # https://rstudio.github.io/leaflet/shiny.html
+  shiny::observeEvent( input$myMap_shape_click, {
+    
+    # store the click(s) over time
+    click <- input$myMap_shape_click
+    
+    # store the polygon ids which are being clicked
+    click.list$ids <- c( click.list$ids, click$id )
+    
+    # filter the spatial data frame
+    # by only including polygons
+    # which are stored in the click.list$ids object
+    lines.of.interest <- worldMap[ which( worldMap$names %in% click.list$ids )]
+    
+    # if statement
+    if( is.null( click$id ) ){
+      # check for required values, if true, then the issue
+      # is "silent". See more at: ?req
+      req( click$id )
+      
+    } else if( !click$id %in% lines.of.interest@data$id ){
+      
+      # call the leaflet proxy
+      leaflet::leafletProxy( mapId = "myMap" ) %>%
+        # and add the polygon lines
+        # using the data stored from the lines.of.interest object
+        addPolylines( data = lines.of.interest
+                      , layerId = lines.of.interest@data$id
+                      , color = "#6cb5bc"
+                      , weight = 5
+                      , opacity = 1
+        ) 
+      
+    } # end of if else statement
+    
+  }) # end of shiny::observeEvent({})
+  
+  
+  # Create the logic for the "Clear the map" action button
+  # which will clear the map of all user-created highlights
+  # and display a clean version of the leaflet map
+  shiny::observeEvent( input$clearHighlight, {
+    
+    # recreate $myMap
+    output$myMap <- leaflet::renderLeaflet({
+      
+      # first
+      # set the reactive value of click.list$ids to NULL
+      click.list$ids <- NULL
+      
+      # second
+      # recall the foundational.map() object
+      foundational.map()
+      
+    }) # end of re-rendering $myMap
+    
+  }) # end of clearHighlight action button logic
   
 }
--- app/ui.R
+++ app/ui.R
@@ -1,17 +1,28 @@
-library(shiny)
-library(leaflet)
-library(dplyr)
-library(leaflet.extras)
+library( leaflet )    
+library( shiny )
+library( shinydashboard )
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
-  
+  # place the contents inside a box
+  shinydashboard::box(
+    width = 12
+    , title = "Click on the map!"
+    # separate the box by a column
+    , column(
+      width = 2
+      , shiny::actionButton( inputId = "clearHighlight"
+                             , icon = icon( name = "eraser")
+                             , label = "Clear the Map"
+                             , style = "color: #fff; background-color: #D75453; border-color: #C73232"
+      )
+    )
+    # separate the box by a column
+    , column(
+      width = 10
+      , leaflet::leafletOutput( outputId = "myMap"
+                                , height = 850
+      )
+    )
+  ) # end of the box
 )
