b1b585008ec15812b63dbae494bde1bfe44c1dbc
Inkalilie <69714176+Inkalilie@users.noreply.github.com>
2020-08-17 18:40:38 +0200

    Update playgroundTanja.R

---
+++ playgroundTanja.R
@@ -0,0 +1,14 @@
+# R Script Tanja
+
+#Libraries:
+#library(dplyr)
+#library(ggplot2)
+#library(magrittr)
+#library(ggmap)
+#library(ggiraph)
+#library(leaflet)
+#library(shiny)
+#library(forecast)
+
+
+# Datentabelle laden
