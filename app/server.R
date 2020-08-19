b010b1265e4860d96928f72b1ec7907c006d4bbc
Nicklas Körtge <nicklas-koertge@web.de>
2020-08-19 18:10:42 +0200

    add clickable Map and related data (geojson)

--- app/server.R
+++ app/server.R
@@ -8,41 +8,30 @@ library( leaflet )
 library( shiny )
 library( shinydashboard )
 library(maps)
+library(sp)
+library(rgdal)
 
 # data <- read.csv("./../wfp_market_food_prices_inUSD.csv")
 
 # import City of Chicago current community area boundaries
-comarea606 <- readRDS( gzcon( url( description = "https://github.com/cenuno/shiny/raw/master/cps_locator/Data/raw-data/comarea606_raw.RDS" ) ) )
-# Note: for speed, I loaded the GeoJSON file from the City's
-#       data portal and exported the object as an RDS file in another script.
-#       To download the raw data yourself, feel free to run this:
-#       install.packages( pkgs = c( "sp", "rgdal" ) )
-#       comarea606 <- 
-#           rgdal::readOGR( dsn = "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=GEOJSON"
-#                              , layer = "OGRGeoJSON"
-#                              , stringsAsFactors = FALSE
-#                             ) 
+worldgeodata <- rgdal::readOGR("./../data/worldWithoutFranchGuinea.geojson")
 
 worldMap = map("world", fill = TRUE, plot = FALSE)
-print(typeof(worldMap))
-print(sapply(worldMap,'[[',1))
 
 
 server <- function(input, output, session) {
-  
   # create foundational map
   foundational.map <- shiny::reactive({
-    leaflet(data = worldMap) %>%
+    leaflet(data = worldgeodata) %>%
       addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
       addPolygons(fillColor = "yellow", stroke = FALSE) %>%
        # setView( lng = -87.567215
        #         , lat = 41.822582
        #         , zoom = 11 ) %>%
-      addPolygons( data = worldMap
+      addPolygons( data = worldgeodata
                    , fillOpacity = 0
-                   , opacity = 0.2
-                   , color = "#000000"
-                   , weight = 2
+                   , opacity = 0.0
+                   , layerId = worldgeodata$name
                    , group = "click.list"
       )
   })
@@ -61,60 +50,45 @@ server <- function(input, output, session) {
   # https://stackoverflow.com/questions/45953741/select-and-deselect-polylines-in-shiny-leaflet
   # https://rstudio.github.io/leaflet/shiny.html
   shiny::observeEvent( input$myMap_shape_click, {
-    
     # store the click(s) over time
     click <- input$myMap_shape_click
-    
     # store the polygon ids which are being clicked
-    click.list$ids <- c( click.list$ids, click$id )
-    
+    click.list$ids <- c( click.list$ids, click$id ) 
     # filter the spatial data frame
     # by only including polygons
     # which are stored in the click.list$ids object
-    lines.of.interest <- worldMap[ which( worldMap$names %in% click.list$ids )]
-    
+    lines.of.interest <- worldgeodata[ which( worldgeodata$name %in% click.list$ids) , ]
     # if statement
     if( is.null( click$id ) ){
       # check for required values, if true, then the issue
       # is "silent". See more at: ?req
       req( click$id )
-      
-    } else if( !click$id %in% lines.of.interest@data$id ){
-      
+    } else if( !click$id %in% lines.of.interest@data$id){
       # call the leaflet proxy
       leaflet::leafletProxy( mapId = "myMap" ) %>%
-        # and add the polygon lines
-        # using the data stored from the lines.of.interest object
-        addPolylines( data = lines.of.interest
-                      , layerId = lines.of.interest@data$id
-                      , color = "#6cb5bc"
-                      , weight = 5
-                      , opacity = 1
-        ) 
+      # and add the polygon lines
+      # using the data stored from the lines.of.interest object
+      addPolylines( data = lines.of.interest,
+                    layerId = lines.of.interest@data$id,
+                    stroke = TRUE,
+                    color = "black",
+                    weight = 1
+      )
       
-    } # end of if else statement
-    
+    }
   }) # end of shiny::observeEvent({})
-  
-  
   # Create the logic for the "Clear the map" action button
   # which will clear the map of all user-created highlights
   # and display a clean version of the leaflet map
   shiny::observeEvent( input$clearHighlight, {
-    
     # recreate $myMap
     output$myMap <- leaflet::renderLeaflet({
-      
       # first
       # set the reactive value of click.list$ids to NULL
       click.list$ids <- NULL
-      
       # second
       # recall the foundational.map() object
       foundational.map()
-      
     }) # end of re-rendering $myMap
-    
   }) # end of clearHighlight action button logic
-  
 }
