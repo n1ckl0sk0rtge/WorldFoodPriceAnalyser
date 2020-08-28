fab4aa7e580047f1956fe2238b626ff125c55080
Nicklas Körtge <nicklas-koertge@web.de>
2020-08-28 16:16:36 +0200

    Build first time user interface

--- app/server.R
+++ app/server.R
@@ -1,19 +1,55 @@
 library(shiny)
+library(leaflet)
+library(maps)
+library(sp)
+library(rgdal)
 
-# data <- read.csv("./../wfp_market_food_prices_inUSD.csv")
+worldgeodata <- readRDS("./../data/worldgeodata.RDS")
 
 server <- function(input, output, session) {
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points())
+  # create foundational map
+  foundational.map <- shiny::reactive({
+    leaflet(data = worldgeodata) %>%
+      setView(lng = 34.933625, lat = 29.903028, zoom = 2.2) %>%
+      addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
+      addPolygons(fillColor = "green", stroke = FALSE) %>%
+      addPolygons( data = worldgeodata
+                   , fillOpacity = 0
+                   , opacity = 0.0
+                   , layerId = worldgeodata$name
+                   , group = "click.list"
+      )
   })
+  
+  
+  output$myMap <- renderLeaflet({
+    foundational.map()
+  })
+  
+  
+  # store the list of clicked polygons in a vector
+  click.list <- shiny::reactiveValues( ids = vector() )
+  
+  
+  
+  shiny::observeEvent( input$myMap_shape_click, {
+    # store the click(s) over time
+    click <- input$myMap_shape_click
+    click.list$ids <- c( click.list$ids, click$id ) 
+    
+    lines.of.interest <- worldgeodata[ which( worldgeodata$name %in% click.list$ids) , ]
+
+    if( is.null( click$id ) ){
+      req( click$id )
+    } else if( !click$id %in% lines.of.interest@data$id){
+      leaflet::leafletProxy( mapId = "myMap" ) %>%
+      addPolylines( data = lines.of.interest,
+                    layerId = lines.of.interest@data$id,
+                    stroke = TRUE,
+                    color = "black",
+                    weight = 1
+      )
+    }
+  }) # end of shiny::observeEvent({})
   
 }
--- app/ui.R
+++ app/ui.R
@@ -1,17 +1,57 @@
 library(shiny)
 library(leaflet)
+library( shinydashboard )
 library(dplyr)
 library(leaflet.extras)
 
-# Define UI for application that draws a histogram
+# Read data from rds-file
+data <- readRDS("./../data/wfp_data.RDS")
+# list countries without duplicates
+countrysWithoutDuplicates <- unique(data$country)
+
+productsWithoutDuplicates <- unique(data$product)
+
+yearsWithoutDuplicates <- strtoi(substr(sort(unique(data$year)), 1, 4))
+
+# define user interface
 ui <- fluidPage(
-  
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("World Food Prices in Developing Countries"),
+  h5(em("created by: Tanja Bühr, Nicklas Körtge, Robert Bilger")),
   
-  # Sidebar with a slider input for number of bins 
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
+  # define site layout  
+  sidebarLayout(
+    
+    sidebarPanel(
+      # define selector for countries
+      #selectInput("select", label = h4("Select Country"), 
+      #            choices = c(list("All countrys" = 1), countrysWithoutDuplicates),
+      #           selected = 1),
+      #
+      #hr(),
+
+      selectInput("select", label = h4("Select product"), 
+                  choices = c(list("All products" = 1), productsWithoutDuplicates), 
+                  selected = 1),
+      
+      hr(),
+      
+      # make a group of checkboxes for years
+      sliderInput("slider2", label = h4("Select years"),
+                  min = head(yearsWithoutDuplicates, n=1), 
+                  max = tail(yearsWithoutDuplicates, n=1), 
+                  step = 1,
+                  value = c(head(yearsWithoutDuplicates, n=1), tail(yearsWithoutDuplicates, n=1)))
+      
+    ),
+    
+    mainPanel(
+      shinydashboard::box(
+        width = 800,
+        leaflet::leafletOutput( outputId = "myMap", height = 850 )
+      )
+    )
+    
+  )
   
 )
