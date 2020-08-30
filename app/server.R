e68018f00ad9a692d75e2da186feadc8b5bb1792
Nicklas Körtge <nicklas-koertge@web.de>
2020-08-30 15:49:04 +0200

    Update User Interface and integrate first plot

--- app/server.R
+++ app/server.R
@@ -3,53 +3,92 @@ library(leaflet)
 library(maps)
 library(sp)
 library(rgdal)
+library(dplyr)
+library(ggplot2)
+
+source("./../functions/foodPriceDevelopmentLineGraph.R")
 
 worldgeodata <- readRDS("./../data/worldgeodata.RDS")
+foodData <- readRDS("./../data/wfp_data.RDS")
 
 server <- function(input, output, session) {
-  # create foundational map
+
   foundational.map <- shiny::reactive({
-    leaflet(data = worldgeodata) %>%
+    
+    foodDataForProduct <- unique(filter(foodData, product == input$selectProducts)$country)
+    
+    countriesWithThoseProducts <- worldgeodata[which(worldgeodata@data$name %in% foodDataForProduct), ]
+    
+    leaflet(data = countriesWithThoseProducts) %>%
       setView(lng = 34.933625, lat = 29.903028, zoom = 2.2) %>%
       addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
-      addPolygons(fillColor = "green", stroke = FALSE) %>%
-      addPolygons( data = worldgeodata
-                   , fillOpacity = 0
-                   , opacity = 0.0
-                   , layerId = worldgeodata$name
-                   , group = "click.list"
+      addPolygons( data = countriesWithThoseProducts,
+                   layerId =  countriesWithThoseProducts@data$name,
+                   fillColor = "green", 
+                   stroke = FALSE,
+                   group = "slectedCountries",
+                   
+                   label =  countriesWithThoseProducts@data$name,
+                   labelOptions = labelOptions(
+                     style = list("font-weight" = "normal", padding = "3px 8px"),
+                     textsize = "15px",
+                     direction = "auto")
       )
   })
-  
   
   output$myMap <- renderLeaflet({
     foundational.map()
   })
   
-  
-  # store the list of clicked polygons in a vector
-  click.list <- shiny::reactiveValues( ids = vector() )
-  
-  
+  slectedCountries <- shiny::reactiveValues( ids = vector() )
   
   shiny::observeEvent( input$myMap_shape_click, {
-    # store the click(s) over time
-    click <- input$myMap_shape_click
-    click.list$ids <- c( click.list$ids, click$id ) 
     
-    lines.of.interest <- worldgeodata[ which( worldgeodata$name %in% click.list$ids) , ]
-
-    if( is.null( click$id ) ){
-      req( click$id )
-    } else if( !click$id %in% lines.of.interest@data$id){
+    click <- input$myMap_shape_click
+    
+    slectedCountry <- worldgeodata[which(worldgeodata@data$name == click$id), ]
+    
+    if(click$id %in% slectedCountries$ids){
+      slectedCountries$ids <- slectedCountries$ids[slectedCountries$ids != click$id ]
+      
       leaflet::leafletProxy( mapId = "myMap" ) %>%
-      addPolylines( data = lines.of.interest,
-                    layerId = lines.of.interest@data$id,
-                    stroke = TRUE,
-                    color = "black",
-                    weight = 1
-      )
+        addPolygons( data = slectedCountry,
+                     layerId = slectedCountry@data$name,
+                     fillColor = "green",
+                     stroke = FALSE,
+                     group = "slectedCountries",
+                     
+                     label = slectedCountry@data$name,
+                     labelOptions = labelOptions(
+                       style = list("font-weight" = "normal", padding = "3px 8px"),
+                       textsize = "15px",
+                       direction = "auto")
+        )
+      
+    } else {
+      slectedCountries$ids <- c(slectedCountries$ids, click$id)  
+      
+      leaflet::leafletProxy( mapId = "myMap" ) %>%
+        addPolygons( data = slectedCountry,
+                     layerId = slectedCountry@data$name,
+                     fillColor = "red",
+                     stroke = FALSE,
+                     group = "slectedCountries",
+                     
+                     label = slectedCountry@data$name,
+                     labelOptions = labelOptions(
+                       style = list("font-weight" = "normal", padding = "3px 8px"),
+                       textsize = "15px",
+                       direction = "auto")
+        )
     }
-  }) # end of shiny::observeEvent({})
+    
+  }) 
+  
+  output$mainPlot <- renderPlot({
+    foodPriceDevelopmentLineGraph(foodData, input, slectedCountries$ids)
+  })
+  
+
   
 }
